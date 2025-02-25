c054fa442ce3149bddead9504df9602f3730cb4f
Asylai <kadyrovaasylai162@gmail.com>
2025-02-25 21:37:19 +0300

    save

---
+++ main.js
@@ -0,0 +1,55 @@
+let FirstUserName = "Alisher";
+let firstUserPoint = 97;
+console.log(`1.${FirstUserName}-${firstUserPoint} из 100 баллов`);
+
+let secondUserName = "Nooruz";
+let secondUserPoint = 89;
+console.log(`1.${secondUserName}-${secondUserPoint} из 100 баллов`);
+
+let thirdUserName = "Jakshylyk";
+let thirdUserPoint = 75;
+console.log(`1.${thirdUserName}-${thirdUserPoint} из 100 баллов`);
+
+let fourthUserName = "Aziret";
+let fourthUserPoint = 90;
+console.log(`1.${fourthUserName}-${fourthUserPoint} из 100 баллов`);
+
+console.log(
+  "Экзамендин орточо баллы: " +
+    (firstUserPoint + secondUserPoint + thirdUserPoint + fourthUserPoint) / 4 +
+    " " +
+    "балл"
+);
+
+let num = 4;
+let num1 = 1 * num;
+let num2 = 2 * num;
+let num3 = 3 * num;
+let num4 = 4 * num;
+let num5 = 5 * num;
+let num6 = 6 * num;
+let num7 = 7 * num;
+let num8 = 8 * num;
+let num9 = 9 * num;
+let num10 = 10 * num;
+
+console.log(`1 × ${num} = ${num1}`);
+console.log(`2 × ${num} = ${num2}`);
+console.log(`3 × ${num} = ${num3}`);
+console.log(`4 × ${num} = ${num4}`);
+console.log(`5 × ${num} = ${num5}`);
+console.log(`6 × ${num} = ${num6}`);
+console.log(`7 × ${num} = ${num7}`);
+console.log(`8 × ${num} = ${num8}`);
+console.log(`9 × ${num} = ${num9}`);
+console.log(`10 × ${num} = ${num10}`);
+
+let a = alert("Вы успешно зарегистрировались!");
+console.log(a);
+
+let b = confirm(
+  "При перезагрузке сайта, введённые данные на форме будут очищены, Вы уверены что хотите перезагрузить?"
+);
+console.log(b);
+
+let c = prompt("Введите ваше имя");
