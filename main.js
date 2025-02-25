6563d7b0c17952bb618844b020ce3cc9367f30f0
Asylai <kadyrovaasylai162@gmail.com>
2025-02-25 21:54:16 +0300

    save

---
+++ main.js
@@ -0,0 +1,58 @@
+// 1) числа от 1 до 100
+// let i = 0;
+// while(i < 100) {
+//     i++;
+//     console.log(i);
+// }
+
+// 2) четные числа от 1 до 100
+// for (let i = 0; i <= 100; i++) {
+// if (i % 2 == 0) console.log(i);
+// }
+
+// 3) сумма четных чисел от 1 до 100
+// let summa = 0;
+// for (let i = 0; i < 100; i++){
+//     summa = summa + i;
+//     if (i % 2 == 0) console.log(i);
+// };
+// console.log(summa);
+
+// 4)
+// let duo = "Duolingo Green";
+// let searchindChar = "e";
+// let long = duo. length;
+// for (let count = 0; count <= long; long--) {
+//     const currentChar = duo.charAt(long);
+//     if (currentChar === searchindChar) {
+//         console.log("Yes,we have a char");
+//     };
+// };
+
+// 5)
+// let fro = "Hello";
+// let stringReverse = "";
+// for (let index = fro.length - 1; index >= 0; index--) {
+//   stringReverse += fro[index];
+// }
+// console.log(stringReverse);
+
+// let fro = "Hello";
+// let stringReverse = "";
+// let index = fro.length - 1;
+// while (index >= 0) {
+//   stringReverse += fro[index];
+//   index--;
+// }
+// console.log(stringReverse);
+
+// 6)
+// let num = 5;
+
+// for (let i = 1; i <= num; i++) {
+//   let res = "";
+//   for (let j = 1; j <= i; j++) {
+//     res += "*";
+//   }
+//   console.log(res);
+// }
